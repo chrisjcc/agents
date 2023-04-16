7254ffea098e581b4dbc4c9881c0a51536ada05d
Christian Contreras <chrisjcc.physics@gmail.com>
2023-04-16 23:02:27 +0200

    update to be more insync with other code

--- src/utils/run.py
+++ src/utils/run.py
@@ -1,14 +1,12 @@
-# Importing necessary libraries
+# Importing necessary libraries (NOT WORKING)
 import os
 import sys
 from typing import Any
 
-import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
-from torch.distributions import Categorical, Normal
+from torch.distributions import Normal
 
 # Add the parent directory of 'agents.src' to the Python path
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,7 +16,7 @@ from models.neural_networks.actor_network import Actor  # type: ignore
 from models.neural_networks.critic_network import Critic  # type: ignore
 
 # Setting the seed for reproducibility
-torch.manual_seed(0)  # TODO: change to 42
+torch.manual_seed(0)
 
 
 if __name__ == "__main__":
@@ -64,8 +62,9 @@ if __name__ == "__main__":
         raise ValueError("Action space shape is None.")
 
     action_dim = int(action_shape[0])
-    max_action = int(action_high[0])
-    action = env.action_space.sample()
+    max_action = float(action_high[0])
+    low = torch.from_numpy(action_space.low)
+    high = torch.from_numpy(action_space.high)
 
     # Actor-Critic hyperparameters
     gamma = 0.99
@@ -85,13 +84,13 @@ if __name__ == "__main__":
     for episode in range(num_episodes):
         # Reset the environment and get the initial state
         state, info = env.reset(seed=42)
-        state_tensor = torch.FloatTensor(state).unsqueeze(0).to(device)
+        state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(device)
 
         episode_reward = 0.0
         done = False
 
         while not done:
-            # with torch.no_grad():
+            #with torch.no_grad():
             # state_tensor /= 255.0  # normalize pixel values from [0, 255] to [0, 1]
             action_mean, action_std = actor(state_tensor)
 
@@ -99,17 +98,12 @@ if __name__ == "__main__":
             action_distribution = Normal(loc=action_mean, scale=action_std)  # type: ignore
             action_tensor = action_distribution.sample()  # type: ignore
 
-            # Rescale the action to the range of the action space
-            rescaled_action = ((action_tensor + 1) / 2) * (
-                action_space.high - action_space.low
-            ) + action_space.low
-
-            # Clip the rescaledaction to ensure it falls within the bounds of the action space
+            # Rescale, then clip the action to ensure it falls within the bounds 
+            # of the action space
             clipped_action = torch.clamp(
-                rescaled_action,
-                torch.from_numpy(action_space.low),
-                torch.from_numpy(action_space.high),
-            )
+            (((action_tensor + 1) / 2) * (high - low) + low), 
+                low, high
+            ).to(device)
 
             # Q-value(s,a) calculation
             q_value = critic(state_tensor, clipped_action)
@@ -120,9 +114,11 @@ if __name__ == "__main__":
             # Take a step in the environment with the chosen action
             next_state, reward, terminated, truncated, info = env.step(action)
 
-            # Compute TD error and update critic network
-            next_state_tensor = torch.FloatTensor(next_state).unsqueeze(0).to(device)
-            # next_state_tensor /= 255.0  # normalize pixel values from [0, 255] to [0, 1]
+            # Convert to tensor
+            next_state_tensor = (
+                torch.tensor(next_state, dtype=torch.float32).unsqueeze(0).to(device)
+            )
+            #next_state_tensor /= 255.0  # normalize pixel values from [0, 255] to [0, 1]
 
             # Select action by subsampling from action space distribution
             next_action_mean, next_action_std = actor(next_state_tensor)
@@ -131,59 +127,65 @@ if __name__ == "__main__":
             next_action_distribution = Normal(loc=next_action_mean, scale=next_action_std)  # type: ignore
             next_action_tensor = next_action_distribution.sample()  # type: ignore
 
-            # Rescale the action to the range of teh action space
-            rescaled_next_action = ((next_action_tensor + 1) / 2) * (
-                action_space.high - action_space.low
-            ) + action_space.low
-
-            # Clip the rescaledaction to ensure it falls within the bounds of the action space
+            # Rescale, then clip the action to ensure it falls within the bounds of the action space
             clipped_next_action = torch.clamp(
-                rescaled_next_action,
-                torch.from_numpy(action_space.low),
-                torch.from_numpy(action_space.high),
-            )
+                (((next_action_tensor + 1) / 2) * (high - low) + low), 
+                low, high
+            ).to(device)
 
             # Q-value(s', a') calculation
             next_q_value = critic(next_state_tensor, clipped_next_action)
 
-            # Calculate advatage (temporal-difference)
-            advantage = reward + gamma * (1 - terminated) * next_q_value - q_value
+            # Calculate target Q-value
+            target_q_value = reward + gamma * (1 - terminated) * next_q_value
 
             # Calculate critic loss
-            critic_loss = advantage**2
+            critic_loss = F.smooth_l1_loss(target_q_value, q_value)
 
-            # Compute advantage and update actor network
-            actor_loss = (
-                -action_distribution.log_prob(clipped_action)  # type: ignore
-                * advantage
-                - entropy_coef * action_distribution.entropy()  # type: ignore
-            )
+            # Calculate advantage
+            advantage = target_q_value - q_value
+
+            # Advantage normalization can improve efficiency of gradient
+            epsilon = 1e-8
+            advantage_norm = (advantage - advantage.mean()) / (advantage.std() + epsilon)
+
+            # Compute entropy
+            entropy = torch.mean(action_distribution.entropy())  # type: ignore
+
+            # Calculate actor loss
+            action_log_prob = next_action_distribution.log_prob(clipped_next_action)  # type: ignore
+            actor_loss = -torch.mean(action_log_prob * advantage_norm)
 
             # Calculate total loss
-            loss = critic_loss + actor_loss.mean()
+            loss = value_coef * critic_loss + actor_loss - entropy_coef * entropy
 
             # Zero out gradients
             critic_optimizer.zero_grad()
             actor_optimizer.zero_grad()
 
             # Calculate backpropagation
-            # actor_loss_scalar.backward()
-            # critic_loss.backward()
             loss.backward()
+
+            # Apply gradient norm clipping
+            torch.nn.utils.clip_grad_norm_(
+                critic.parameters(), max_norm=0.5, norm_type=2
+            )
+            torch.nn.utils.clip_grad_norm_(
+                actor.parameters(), max_norm=0.5, norm_type=2
+            )
 
             # Update network weights
             critic_optimizer.step()
             actor_optimizer.step()
 
-            episode_reward += float(reward)
-
             state = next_state
+            state_tensor = next_state_tensor
 
-            print(f"Episode's cummulative reward: {episode_reward:.2f}")
+            episode_reward += float(reward)
+            print(f"Total reward: {episode_reward:.2f}")
 
             # Update if the environment is done
             done = terminated or truncated
-
             if done:
                 break
 
